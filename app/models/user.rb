fe6f9946e32de203d467ca81ed72bf3f3bf6dbbe
nafi durmus <nafidurmus07@gmail.com>
2017-11-17 20:25:31 +0300

    Add comments to posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :posts #bir kullanıcınınn birde n fazla post oluşturabileceğini belirttik.
+  has_many :comments 
 end
